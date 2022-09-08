84c42fb688fda8e4fdd2935259d73d3fc6186329
vasile.mihali <vasile.mihali@modmed.com>
2022-09-08 12:29:38 +0300

    Fist Commit - project base

---
+++ src/test/java/com/synergo/deliverybe/DeliveryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synergo.deliverybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
